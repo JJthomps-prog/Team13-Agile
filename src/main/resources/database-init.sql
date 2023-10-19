ea5c9e0c3df8bb23bfde9be41507f2dbf4e833df
yclover <1031092112@qq.com>
2023-10-19 15:24:05 -0400

    initialize the database

--- src/main/resources/database-init.sql
+++ src/main/resources/database-init.sql
@@ -1,8 +1,13 @@
+DROP TABLE IF EXISTS users;
+
 CREATE TABLE users
 (
-    id SERIAL PRIMARY KEY  NOT NULL,
+    id SERIAL PRIMARY KEY NOT NULL,
     password TEXT NOT NULL,
     enabled BOOLEAN DEFAULT TRUE NOT NULL,
     first_name TEXT,
     last_name TEXT
 );
+
+INSERT INTO users (id, password, enabled, first_name, last_name)
+VALUES (11, 'secret', TRUE, 'sj', 'yu');
