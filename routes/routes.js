3815962ac6b27e6481269e3f33cf6438939efeb9
钦越涵 <qinyiqinyuehan@qq.com>
2023-10-28 13:25:23 -0400

    okay

---
+++ routes/routes.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+
+// Events Page
+router.get('/categories/events', (req, res) => {
+  const eventData = [
+    { title: 'Event Title 1', date: 'January 1, 2024', time: '6:00 PM - 9:00 PM', location: 'Venue ABC', description: 'Lorem ipsum...' },
+    { title: 'Event Title 2', date: 'February 15, 2024', time: '2:00 PM - 5:00 PM', location: 'Venue XYZ', description: 'Duis aute...' },
+  ];
+
+  res.render('events', { events: eventData });
+});
+
+// Latest Topics Page
+router.get('/latest-topic', (req, res) => {
+  const topicsData = [
+    { title: 'Topic Title 1', author: 'John Doe', replies: 5, content: 'That sounds like a fun event to attend!' },
+    { title: 'Topic Title 2', author: 'Jane Smith', replies: 8, content: 'I tried the new restaurant last week, and it was fantastic!' },
+    // Add more topics here
+  ];
+
+  res.render('latest-topic', { topics: topicsData });
+});
+
+// Popular Topics Page
+router.get('/latest-topic/popular-topics', (req, res) => {
+  // Fetch and render popular topics here
+});
+
+// Posts Page
+router.get('/categories/posts', (req, res) => {
+  res.render('posts');
+});
+
+// Landmarks Page
+router.get('/categories/landmarks', (req, res) => {
+  res.render('landmarks');
+});
+
+// Login Page
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// Register Page
+router.get('/register', (req, res) => {
+  res.render('register');
+});
+
+module.exports = router;
